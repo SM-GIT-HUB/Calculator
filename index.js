d43c2de76a60f5ecf291a3b93879ce238d1c76ab
Soumik Majumder <soumikcode02@gmail.com>
2024-07-01 14:36:10 +0530

    calculator code done

---
+++ index.js
@@ -0,0 +1,24 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(str)
+{
+    display.value += str;
+}
+
+function clearDisplay()
+{
+    display.value = "";
+}
+
+function calculate()
+{
+    if (display.value == "") {
+        return;
+    }
+    try {
+        display.value = eval(display.value);
+    }
+    catch(error) {
+        display.value = "Error";
+    }
+}
